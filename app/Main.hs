5abc35298f254c13b462155cc1061846ffe87561
araujoviana <63423848+araujoviana@users.noreply.github.com>
2025-01-16 20:54:57 -0300

    Reconsider data types

--- app/Main.hs
+++ app/Main.hs
@@ -1,7 +1,19 @@
 module Main where
-data Mark = X | O
 
-type Board = [[Mark]]
+import Data.Char (toUpper)
+import Data.List
+import Data.Maybe (fromMaybe)
+import Text.Read (readMaybe)
+
+data Mark = X | O deriving (Show, Read, Eq, Ord)
+
+-- Tic-Tac-Toe Board
+---------------
+-- [[O,X,X], --
+--  [X, ,O], --
+--  [X,O, ]] --
+---------------
+type Board = [[Maybe Mark]] -- Any tile could be empty
 
 main :: IO ()
 main = putStrLn "Hello, Haskell!"
